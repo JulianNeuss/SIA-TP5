7d8d76bf019634c82c24fa592402bddd938ae6f8
reurison-sr <43077478+reurison-sr@users.noreply.github.com>
2021-06-15 19:33:05 -0300

    update multicapa.py

--- multicapa.py
+++ multicapa.py
@@ -3,6 +3,9 @@ from random import random
 from math import exp
 import numpy as np
 
+def stop():
+	int(input(':>'))
+
 def crear_red(num_entradas, camada_escondida, num_salidas):
 
 	red = list()
@@ -19,11 +22,19 @@ def crear_red(num_entradas, camada_escondida, num_salidas):
 	return red
 
 def activacion(pesos, inputs):
+
 	bias = pesos[-1] #El bias es el ultimo elemento
-	activation = bias 
+	activation = bias*1 
 	for i in range(len(pesos)-1): #for i in [xi,yi]
 		activation += pesos[i] * inputs[i] #x1*w1 + x2*w2 + ... + xn*wn + bias
 	return activation
+
+def sigmoid(activation):
+    return (1/(1+np.exp(-activation)))
+
+def sigmoid_dx_dt(output):
+	dg_dx = output*(1.0-output)
+	return dg_dx
 
 def tan_hiperbolica(activation, beta=0.5):
 	#tangente hiperbolica
@@ -36,68 +47,94 @@ def tanh_dx_dt(output, beta=0.5):
 	return dg_dx
 
 def propagacion(red, entrada):
+
 	espacio_latente = []
-	inputs = entrada # (xi, yi, salida_i)
+	inputs = entrada 
+
 	for capa in red: #Nivel de la red.
+
 		n_entradas = []
+
 		for neurona in capa: #
+
 			#Unidad de salida alcanza un estado de exitación h_{i}^{mu}
 			activation_ = activacion(neurona['pesos'], inputs) #Pesos de V1 (w11, w21, 1) con input (xi,yi,Salida_i)
 			#La salida para el neurônio V1 = sigmoide(w11*x1 + w21*x2 + 1)
-			neurona['salida'] = tan_hiperbolica(activation_) #Diap : 10/49
+			neurona['salida'] = tan_hiperbolica(activation_)
+
 			if len(capa) == 2:
 				espacio_latente.append(tan_hiperbolica(activation_))
+			
 			n_entradas.append(neurona['salida'])
+
 		inputs = n_entradas
 
 	return inputs, espacio_latente
 
-def backtracking(red, expected, beta=0.5): 
-	for i in reversed(range(len(red))): #Empezamos en la camada de salida
-		capa = red[i] #O1, O2
 
-		for j in range(len(capa)): 
-			Neurona_J = capa[j] #O1 = {'pesos':[]}, O2 = {'pesos':[]}
-			error = 0.0
+def backtracking(red, expected):
 
-			if i != len(red)-1: 
-				for Neurona_i in red[i + 1]:
-					error += (Neurona_i['pesos'][j] * Neurona_i['delta'])
-			else: 
-				error = expected[j] - Neurona_J['salida']
+	for i in reversed(range(len(red))):
+		#Empezamos por la ultima camada
 
-			Neurona_J['error'] = error
-			Neurona_J['delta'] = error * tanh_dx_dt(Neurona_J['salida']) #diap 14/49 : ultima ecuación
+		layer = red[i]
+		errors = list()
+
+		if i != len(red)-1:
+			for j in range(len(layer)):
+				error = 0.0
+				for neuron in red[i + 1]:
+					error += (neuron['pesos'][j] * neuron['delta'])
+				errors.append(error)
+		else:
+
+			for j in range(len(layer)):
+				neuron = layer[j]
+				errors.append(expected[j] - neuron['salida'])
+				
+		for j in range(len(layer)):
+			neuron = layer[j]
+			neuron['delta'] = errors[j] * tanh_dx_dt(neuron['salida'])
+
+
 			
-
 def actualizar_pesos(red, row, factor_aprendizaje):
+
 	for i in range(len(red)):
-		inputs = row[:-1]
+		
+		inputs = row #Tenho que usar isso pq não tenho classificação.
+
 		if i != 0:
 			inputs = [neurona['salida'] for neurona in red[i - 1]]
+
 		for neurona in red[i]:
 			for j in range(len(inputs)):
-				neurona['pesos'][j] += factor_aprendizaje * neurona['delta'] * inputs[j]
+				neurona['pesos'][j] += factor_aprendizaje * neurona['delta']*inputs[j]
 			neurona['pesos'][-1] += factor_aprendizaje * neurona['delta']
 
 
-def entrenar_red(red, set_entrenamiento, factor_aprendizaje, epochs, num_salidas, COTA_ERROR):
+def entrenar_red(red, set_entrenamiento, factor_aprendizaje, epochs, COTA_ERROR):
+
 	latentes = []
 	espacio_latente = []
 	count_epochs = 0
 	Error_w = 1
 
 	while (Error_w > COTA_ERROR):
+
 		Error_w = 0 #Funcion de costo E(W)
 		espacio_latente = []
-		for row in set_entrenamiento: # (xi, yi, salida_i)
-			salida, el = propagacion(red, row) #Entrada -> Salida
+		
+		for row in set_entrenamiento:
 
+			#Forward propagation
+			salida, el = propagacion(red, row)
 			espacio_latente.append(el)
 			salida_deseada = row
 
 			#Error entre la salida deseada e la salida calculada (Diap : 11/49)
-			Error_w += 0.5*sum([(salida_deseada[i]-salida[i])**2 for i in range(len(salida_deseada))])
+			Error_w += ((np.linalg.norm(np.array(salida_deseada) - np.array(salida)))**2)*0.5
+
 			backtracking(red, salida_deseada)
 			actualizar_pesos(red, row, factor_aprendizaje)
 
@@ -109,24 +146,24 @@ def entrenar_red(red, set_entrenamiento, factor_aprendizaje, epochs, num_salidas
 
 	return espacio_latente
 
-
 def predict(red, entrada):
-	outputs = propagacion(red, entrada)
-	return float(outputs[0])
-
+	outputs,latente = propagacion(red, entrada)
+	#propagacion retorna : inputs y espacio_latente
+	#return float(outputs[0])
+	return outputs, latente
 
 
 def main():
 
 	num_entradas  = len(dataset_TRAIN2[0]) - 1 #35
 	num_salidas   = 1
-	capas_escondidas   = [4,3]
+	capas_escondidas   = [2]
 	COTA_ERROR         = 0.002 
-	factor_aprendizaje = 0.01
+	factor_aprendizaje = 0.001
 	epochs             = 20000
 
 	red = crear_red(num_entradas, capas_escondidas, num_salidas)
-	entrenar_red(red, dataset_TRAIN2, factor_aprendizaje, epochs, num_salidas, COTA_ERROR)
+	entrenar_red(red, dataset_TRAIN2, factor_aprendizaje, epochs, COTA_ERROR)
 
 	#Classificacion
 	for data in dataset_TEST2:
