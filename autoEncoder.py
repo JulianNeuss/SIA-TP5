244c239602f905c685ffb9a0b79b3e24288eb844
reurison-sr <43077478+reurison-sr@users.noreply.github.com>
2021-06-16 15:19:54 -0300

    add plot MSE

--- autoEncoder.py
+++ autoEncoder.py
@@ -1,17 +1,25 @@
+import sys
+from scipy import optimize
+import numpy as np
 from multicapa import *
-from font3 import *
 from FontNew import *
 from numpy import random
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
-from statistics import mean
-import statistics
-import numpy as np
-from functools import reduce
-import sys
+from red_entrenada import *
+
+#def f(x):
+#   return x**2
+#minimum = optimize.fmin_powell(f, -1)
+
 
 def stop():
     int(input(":>"))
+
+
+def plot_graph(color):
+    plt.plot(epocas, MSE, c=color, lw='1')
+
 
 def main():
 
@@ -28,55 +36,76 @@ def main():
     k = [1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1,0,1,0,0,1,1,0,0,0,1,0,1,0,0,1,0,0,1,0]
     l = [0,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0]
 
-    autoencoder = crear_red(35,[25, 7, 2, 7,5],35)
+    #a_1b = [1,0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,1,1,0,1,1,0,0,1,1,1,0,0,1,1,0,1,1,0,1] #1 bit change
+    #a_2b = [0,0,1,0,0,0,1,1,1,0,0,0,0,0,1,0,1,1,0,1,1,0,0,1,1,1,0,0,1,1,0,1,0,0,1] #2 bit change
+    #a_3b = [0,1,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,1,0,1,1,0,0,1,0,1,0,0,1,1,0,1,1,0,1] #3 bit change 
+    #a_4b = [0,0,0,1,0,0,1,1,0,0,0,0,0,0,1,0,1,1,0,1,1,0,0,1,1,1,0,0,1,0,0,1,1,0,0] #4 bit change
+    #a_5b = [1,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,1,1,0,1,1,0,0,0,1,1,0,0,1,0,0,1,1,1,1] #5 bit change
 
-
-    COTA_ERROR         = 0.1
-    factor_aprendizaje = 0.01
+    autoencoder = crear_red(35,[15,2,15],35)
+    COTA_ERROR         = 0.001
+    factor_aprendizaje = 0.005
+    momentum           = 0.8
     epochs             = 40000
 
+    stop()
+
     print("\n\n Entrenar \n\n")
-    espacio_saliente = entrenar_red(autoencoder, Font3, factor_aprendizaje, epochs, COTA_ERROR)
+    espacio_saliente, MSE, epocas = entrenar_red(autoencoder, Font, factor_aprendizaje, momentum, epochs, COTA_ERROR)
     print("\n\n Post entrenar \n\n")
 
+    plot_graph('red')
+    plt.ylabel('Error')
+    plt.xlabel('Epocas')
+    plt.show()
+    plt.clf()
 
 
-    #Predizer a,b,c,d
-    prediction_a, latente = predict(autoencoder, a)
-    prediction_b, latente = predict(autoencoder, b)
-    prediction_c, latente = predict(autoencoder, c)
-    prediction_d, latente = predict(autoencoder, d)
+    #print(autoencoder)
+    #stop()
+    #autoencoder = red
 
-    prediction_round_a = np.rint(prediction_a)
-    prediction_round_a = prediction_round_a.astype(int) #convert from float to int
+    #prediction_1b, latente = predict(autoencoder, a_1b)
+    #prediction_2b, latente = predict(autoencoder, a_2b)
+    #prediction_3b, latente = predict(autoencoder, a_3b)
+    #prediction_4b, latente = predict(autoencoder, a_4b)
+    #prediction_5b, latente = predict(autoencoder, a_5b)
 
-    prediction_round_b = np.rint(prediction_b)
-    prediction_round_b = prediction_round_b.astype(int) #convert from float to int
+    #prediction_round_1b = np.rint(prediction_1b)
+    #prediction_round_1b = prediction_round_1b.astype(int) #convert from float to int
+    #prediction_round_2b = np.rint(prediction_2b)
+    #prediction_round_2b = prediction_round_2b.astype(int) #convert from float to int
+    #prediction_round_3b = np.rint(prediction_3b)
+    #prediction_round_3b = prediction_round_3b.astype(int) #convert from float to int
+    #prediction_round_4b = np.rint(prediction_4b)
+    #prediction_round_4b = prediction_round_4b.astype(int) #convert from float to int
+    #prediction_round_5b = np.rint(prediction_5b)
+    #prediction_round_5b = prediction_round_5b.astype(int) #convert from float to int
 
-    prediction_round_c = np.rint(prediction_c)
-    prediction_round_c = prediction_round_c.astype(int) #convert from float to int
+    #print('a         : {0}'.format(a))
+    #print('a_1b  : {0}'.format(list(prediction_round_1b)))
+    #print("||a - a_1b|| = ", np.linalg.norm(np.array(a) - np.array(prediction_1b)))
+    #print('\n')
 
-    prediction_round_d = np.rint(prediction_d)
-    prediction_round_d = prediction_round_d.astype(int) #convert from float to int
+    #print('a     : {0}'.format(a))
+    #print('a_2b  : {0}'.format(list(prediction_round_2b)))
+    #print("||a - a_2b|| = ", np.linalg.norm(np.array(a) - np.array(prediction_2b)))
+    #print('\n')
 
-    print('a     : {0}'.format(a))
-    print('a_hat : {0}'.format(list(prediction_round_a)))
-    print("||a - a_hat|| = ", np.linalg.norm(np.array(a) - np.array(prediction_round_a)))
-    print('\n')
+    #print('a     : {0}'.format(a))
+    #print('a_3b  : {0}'.format(list(prediction_round_3b)))
+    #print("||a - a_3b|| = ", np.linalg.norm(np.array(a) - np.array(prediction_3b)))
+    #print('\n')
 
-    print('b     : {0}'.format(b))
-    print('b_hat : {0}'.format(list(prediction_round_b)))
-    print("||b - b_hat|| = ", np.linalg.norm(np.array(b) - np.array(prediction_round_b)))
-    print('\n')
+    #print('a     : {0}'.format(a))
+    #print('a_4b  : {0}'.format(list(prediction_round_4b)))
+    #print("||a - a_4b|| = ", np.linalg.norm(np.array(a) - np.array(prediction_4b)))
+    #print('\n')
 
-    print('c     : {0}'.format(c))
-    print('c_hat : {0}'.format(list(prediction_round_c)))
-    print("||c - c_hat|| = ", np.linalg.norm(np.array(c) - np.array(prediction_round_c)))
-    print('\n')
-
-    print('d     : {0}'.format(d))
-    print('d_hat : {0}'.format(list(prediction_round_d)))
-    print("||d - d_hat|| = ", np.linalg.norm(np.array(d) - np.array(prediction_round_d)))
+    #print('a     : {0}'.format(a))
+    #print('a_5b  : {0}'.format(list(prediction_round_5b)))
+    #print("||a - a_5b|| = ", np.linalg.norm(np.array(a) - np.array(prediction_5b)))
+    #print('\n')
 
 
 
